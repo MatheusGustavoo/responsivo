1c9ff4e79a8d1b7a2472fca4f9e797d31765be72
mbrplay <56611686+mbrplay@users.noreply.github.com>
2021-05-26 16:25:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function executar1(){
 const imagens = document.querySelectorAll('.js-div li')
 const frase = document.querySelectorAll('.descr-1 img')
 frase[0].classList.add('ativo')
@@ -16,3 +17,49 @@ imagens.forEach((img, index) =>{
 })
 
 console.log(imagens.index)
+}
+executar1();
+
+
+function executar2(){
+const linkIN =  document.querySelectorAll('.option a')
+
+function scrollToSection(event){
+ event.preventDefault();
+ const href= event.currentTarget.getAttribute('href');
+ const section = document.querySelector(href)
+ const topo = section.offsetTop;
+
+ section.scrollIntoView({
+  behavior:'smooth',
+  block: 'start',
+
+ })
+ 
+}
+
+
+linkIN.forEach((item) =>{
+  item.addEventListener('click', scrollToSection)
+})
+
+}
+
+executar2();
+
+const sections = document.querySelectorAll('.js-scroll');
+const apartamentos = document.querySelectorAll('.aps')
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if(sectionTop < 0){
+      apartamentos.forEach((ap) =>{
+      ap.classList.add('ativar-scroll')})
+    }
+  })
+
+    
+}
+
+window.addEventListener('scroll', animaScroll);  
